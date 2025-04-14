acdd6deebf40ebb4e2e3e324107cd2e66c77f2b5
02Roland <yvessiewe09@gmail.com>
2025-04-14 13:05:51 +0100

    modification de form et app

--- src/composants/form.jsx
+++ src/composants/form.jsx
@@ -1,11 +1,18 @@
 function LabelParent({labelTitle}){
     return(
         <div>
-            <label htmlFor={labelTitle}></label>
+            <label>{labelTitle}</label>
             <input type="text" />
         </div>
     )
 }
+/**
+ * A functional component that renders a form with a title and input fields.
+ *
+ * @param {Object} props - The props object.
+ * @param {string} props.TitleForm - The title of the form to be displayed as an <h1>.
+ * @returns {JSX.Element} The rendered form component.
+ */
 export function Form({TitleForm}){
     return(
         <form action="">
